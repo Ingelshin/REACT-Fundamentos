d95d5733aa04b8d9ab0856855cfa856284a3d6bd
Ingelshin <ingelshin.barraza@gmail.com>
2019-12-25 12:13:25 -0500

    Componente Básico

--- src/index.js
+++ src/index.js
@@ -1,29 +1,7 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import Card from './components/Card'
 
-const user = {
-  firstName: "Ingelshin",
-  lastName: "Barraza",
-  avatar: "https://image.flaticon.com/icons/png/512/147/147140.png"
-}
-
-function getName(user){
-  return `${user.firstName} ${user.lastName}`
-}
-
-function getGreeting(user){
-  if (user) {
-    return <h1>Hello {getName(user)}</h1>
-  }
-  return <h1>Hello Stranger</h1>
-}
-
-const element = (
-  <div>
-    <h2>{getGreeting(user)}</h2>
-    <img src={user.avatar} alt="avatar"/>
-  </div>
-)
 const container = document.getElementById("root")
 
-ReactDOM.render(element, container);
+ReactDOM.render(<Card/>, container);
