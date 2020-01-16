9595b78282a78e21480ce714fdd066b96592050b
Ingelshin <55919498+Ingelshin@users.noreply.github.com>
2020-01-16 17:32:59 -0500

    .

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,7 +1,22 @@
-import React from 'react'
+import React, {Component} from 'react'
 import ExerciseImg from '../images/exercise.png'
 
-class Card extends React.Component {
+class Text extends Component {
+  render () {
+    return (
+      <div>
+        <p>{this.props.description}</p>
+        <p>{this.props.valores.map(this.props.multiply)}</p>
+      </div>
+    )
+  }
+}
+
+Text.defaultProps = {
+  description: "Descripción por defecto"
+}
+
+class Card extends Component {
   render(){
     return (
       <div>
@@ -10,7 +25,11 @@ class Card extends React.Component {
         </div>
         <div>
           <h1>Technique Guides</h1>
-          <p>Learn amazing street workout and calisthenics</p>
+          <Text 
+            description="Learn amazing street workout and calisthenics"
+            valores={[1, 2, 3]}
+            multiply={(number) => number*2}
+          />
         </div>
       </div>
     )
