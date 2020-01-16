f6d76f85332de99371182d90b818f1a2176c9d0a
Ingelshin <55919498+Ingelshin@users.noreply.github.com>
2020-01-16 18:55:57 -0500

    state

--- src/index.js
+++ src/index.js
@@ -1,17 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import Card from './components/Card'
-import Mensaje from './components/Mensaje';
+import App from './components/App'
 
 const container = document.getElementById("root")
-const mensaje = document.getElementById("mensaje")
 
-ReactDOM.render(<Card
-  title="Techniques Fitness"
-/>, container)
-
-ReactDOM.render(<Mensaje
-  size="30px"
-  color="blue"
-  contenido="Hola Ingelshin"
-/>, mensaje)
+ReactDOM.render(<App/>, container)
